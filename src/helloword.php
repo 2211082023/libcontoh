9b4720f562b5148be34cc1a523511d18bb73adfd
Nurul Aulia <nurulaulialubis03@gmail.com>
2024-05-29 09:22:29 +0700

    first commit

---
+++ src/helloword.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace nurul\libhello;
+
+class helloword{
+    public function sayHello($name){
+        return"Hello $name welcome to php and composer to create your own library";
+    }
+}
